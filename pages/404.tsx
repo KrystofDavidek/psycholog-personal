f7a0992268050ed435bbfcc8a875ea44512be7e4
Krystof Davidek <kry.davidek@gmail.com>
2022-01-09 17:09:32 +0100

    init project

---
+++ pages/404.tsx
@@ -0,0 +1,7 @@
+export default function Custom404() {
+  return (
+    <div className="flex flex-col justify-center min-h-screen">
+      <h1 className="p-5 text-center">404 - Page Not Found</h1>
+    </div>
+  );
+}
